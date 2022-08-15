ab62faf006718568554e3224c8cc0ffb1d3b8bb0
Michel Alves <malvesdesign@hotmail.com>
2022-08-15 10:50:33 -0300

    Testes

--- src/main/java/com/malves/parking/service/ParkingService.java
+++ src/main/java/com/malves/parking/service/ParkingService.java
@@ -9,21 +9,13 @@ import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
+import com.malves.parking.exception.ParkingNotFoundException;
 import com.malves.parking.model.Parking;
 
 @Service
 public class ParkingService {
 
 	private static Map<String, Parking> parkingMap = new HashMap<>();
-	
-	static {
-		var id = getUUID();
-		var id1 = getUUID();
-		Parking parking = new Parking(id, "DMS-1122", "SC", "CELTA", "PRETO");
-		Parking parking1 = new Parking(id1, "WAS-1234", "SP", "VW GOL", "VERMELHO");
-		parkingMap.put(id, parking);
-		parkingMap.put(id1, parking1);
-	}
 	
 	private static String getUUID() {
 		return UUID.randomUUID().toString().replace("-", "");
@@ -34,7 +26,11 @@ public class ParkingService {
 	}
 
 	public Parking findById(String id) {
-		return parkingMap.get(id);
+		Parking parking = parkingMap.get(id);
+		if (parking == null) {
+			throw new ParkingNotFoundException(id);
+		}
+		return parking;
 	}
 
 	public Parking create(Parking parkingCreate) {
@@ -43,5 +39,24 @@ public class ParkingService {
 		parkingCreate.setEntryDate(LocalDateTime.now());
 		parkingMap.put(uuid, parkingCreate);
 		return parkingCreate;
+	}
+
+	public void delete(String id) {
+		findById(id);
+		parkingMap.remove(id);
+	}
+
+	public Parking update(String id, Parking parkingUpdate) {
+		Parking parking = findById(id);
+		parking.setColor(parkingUpdate.getColor());
+		parkingMap.replace(id, parking);
+		return parking;
+	}
+
+	public Parking exit(String id) {
+		// recuperar o estacionado
+		// autualizar data de saida
+		// calcular o valor
+		return null;
 	}
 }
---
+++ src/test/java/com/malves/parking/controller/ParkingControllerTest.java
@@ -0,0 +1,60 @@
+package com.malves.parking.controller;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import com.malves.parking.controller.dto.ParkingCreateDTO;
+
+import io.restassured.RestAssured;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class ParkingControllerTest {
+	
+	@LocalServerPort
+	private int randomPort;
+	
+	@BeforeEach
+	public void setUpTest() {
+		System.out.println(randomPort);
+		RestAssured.port = randomPort;
+	}
+
+	@Test
+	void whenFindAllThenCheckResult() {
+		RestAssured.given()
+			.when()
+			.get("/parkings")
+			.then()
+			.statusCode(HttpStatus.OK.value());
+//			.body("license[0]", Matchers.equalTo("WRT-5555"));
+//			.extract().response().body().prettyPrint();
+	}
+	
+	@Test
+	void whenCreateThenCheckIsCreated() {
+		
+		ParkingCreateDTO createDTO = new ParkingCreateDTO();
+		createDTO.setColor("AMARELO");
+		createDTO.setLicense("WRT-5555");
+		createDTO.setModel("BRASILIA");
+		createDTO.setState("SP");
+		
+		RestAssured.given()
+		.when()
+		.contentType(MediaType.APPLICATION_JSON_VALUE)
+		.body(createDTO)
+		.post("/parkings")
+		.then()
+		.statusCode(HttpStatus.CREATED.value())
+		.body("license", Matchers.equalTo("WRT-5555"))
+		.body("color", Matchers.equalTo("AMARELO"));
+	}
+	
+	
+}
